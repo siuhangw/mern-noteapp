e0a733c3d5d9a60ca5725dcae8d4895134362f87
siuhangw <wshiahb@gmail.com>
2025-09-22 16:48:25 +0800

    Prepare for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import NotesRouter from "./routes/noteRoutes.js"
 import connectDB from "./config/db.js"
@@ -8,27 +9,37 @@ import ratelimiter from "./middleware/rateLimiter.js"
 
 const app = express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 dotenv.config()
 
 // Middleware
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
-  }
-));
+if(process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }));
+}
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log(`Request ${req.method} ${req.url}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`Request ${req.method} ${req.url}`);
+//   next();
+// });
 app.use(ratelimiter);
 
 // API
-app.get('/', (req, res) => {
-  res.status(200).send("Welcome to My Notes!")
-});
+// app.get('/', (req, res) => {
+//   res.status(200).send("Welcome to My Notes!")
+// });
 app.use('/api/notes', NotesRouter)
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
+
 connectDB().then(()=>{
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}👂`)
